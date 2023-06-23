6abf8af864cc79fc9ea552dd06126746af2e9eec
ciaobellala <bellagoodwin@protonmail.com>
2023-06-23 13:46:15 -0400

    change logo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="butterfly logo" class="logo" src="./assets/logob.svg" width="170" height="170" />
 
     <div class="wrapper">
       <HelloWorld msg="Bella Goodwin's Website" />
