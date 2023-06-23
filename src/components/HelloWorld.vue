b65659b5cf1baf91f42bc4df3dc34acfab9b0582
ciaobellala <bellagoodwin@protonmail.com>
2023-06-23 13:39:36 -0400

    drop down menu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,8 +8,8 @@ defineProps<{
   <div class="greetings">
     <h1 class="purple">{{ msg }}</h1>
     <h3>
-      Bella's Web Project 
-      <a href="https://www.instagram.com/baked._.bybella/" target="_blank" rel="noopener"><img alt="Baking Instagram" class="logo" src="/src/assets/logoInsta.svg"  width="125" height="125"></a> 
+      Baked By Bella 
+      <a href="https://www.instagram.com/baked._.bybella/" target="_blank" rel="noopener"><img alt="Baking Instagram" class="logo" src="/src/assets/logoInsta.svg"  width="75" height="75"></a> 
     </h3>
   </div>
 </template>
