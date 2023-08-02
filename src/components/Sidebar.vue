ba159eecacc109d31a43f622842eebc9616e19d2
ciaobellala <bellagoodwin@protonmail.com>
2023-08-02 14:22:49 -0400

    error in sidebar fixed

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,7 +4,7 @@
 <ul>
     <li class="dropdown">
       <a href="index.html" class="dropbtn"><img alt="home logo" class="logo" src="/src/assets/logoHome.svg" width="50" height="50" /></a>
-      <div class="dropdown-content" @click="current">
+      <div class="dropdown-content" >
         <a href="index.html">Home</a>
         <a href="contact.html">Contact</a>
         <a href="baking.html">Baking</a>
